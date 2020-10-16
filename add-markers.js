fea5ed63da255390be3f9416e00826713b64a3ee
FelixTellmann <felix@tellmann.co.za>
2020-10-16 09:02:11 +0200

    Added Line Highlight functionality

--- add-markers.js
+++ add-markers.js
@@ -1,63 +1,20 @@
-/**
- * Code <s>copied</s> inspired from from:
- * https://github.com/rexxars/react-refractor/blob/master/src/addMarkers.js
- */
-/**
- * Code <s>copied</s> inspired from from:
- * https://github.com/rexxars/react-refractor/blob/master/src/addMarkers.js
- */
-const lineNumberify = function lineNumberify(ast, lineNum = 1) {
-  let lineNumber = lineNum;
-  return ast.reduce(
-    (result, node) => {
-      if (result.lineNumber) {
-        lineNumber = result.lineNumber;
-      }
-      if (node.type === 'text') {
-        if (node.value.indexOf('\n') === -1) {
-          node.lineNumber = lineNumber;
-          result.nodes.push(node);
-          return result;
-        }
-
-        const lines = node.value.split('\n');
-        for (let i = 0; i < lines.length; i++) {
-          result.nodes.push({
-            type: 'text',
-            value: i === lines.length - 1 ? lines[i] : `${lines[i]}\n`,
-            lineNumber: i === 0 ? lineNumber : ++lineNumber
-          });
-        }
-
-        result.lineNumber = lineNumber;
-        return result;
-      }
-
-      if (node.children) {
-        node.lineNumber = lineNumber;
-        const processed = lineNumberify(node.children, lineNumber);
-        node.children = processed.nodes;
-        result.lineNumber = processed.lineNumber;
-        result.nodes.push(node);
-        return result;
-      }
-
-      result.nodes.push(node);
-      return result;
-    },
-    { nodes: [], lineNumber: lineNumber }
-  );
-};
-
 const makeLine = (markers, line, children, options, classNames = []) => {
   if (typeof classNames === 'string') {
     classNames = [classNames];
   }
 
   function extractWhitespace(children) {
-    if (children[0].type === 'text' && /^\s+$/.test(children[0].value)) {
+    if (
+      children &&
+      children[0].type === 'text' &&
+      /^\s+$/.test(children[0].value)
+    ) {
       return children.splice(0, 1)[0];
-    } else if (children[0].type === 'text' && /^\s+/.test(children[0].value)) {
+    } else if (
+      children &&
+      children[0].type === 'text' &&
+      /^\s+/.test(children[0].value)
+    ) {
       const returnObject = {
         type: 'text',
         value: /^\s+/.exec(children[0].value)[0]
@@ -65,6 +22,7 @@ const makeLine = (markers, line, children, options, classNames = []) => {
       children[0].value = children[0].value.replace(/^\s+/, '');
       return returnObject;
     } else if (
+      children &&
       children[0] &&
       children[0].children &&
       children[0].children.length > 0
@@ -81,11 +39,7 @@ const makeLine = (markers, line, children, options, classNames = []) => {
     type: 'element',
     tagName: 'div',
     properties: {
-      className: [
-        `line`,
-        markers[line].highlight ? 'line-highlight' : '',
-        ...classNames
-      ]
+      className: [`line`, markers[line].highlight ? 'line-highlight' : '']
     },
     children: [
       {
@@ -114,7 +68,7 @@ const makeLine = (markers, line, children, options, classNames = []) => {
         ? {
             type: 'element',
             tagName: 'span',
-            properties: { className: 'line-content' },
+            properties: { className: ['line-content', ...classNames] },
             children: [...children],
             lineNumber: markers[line].line
           }
@@ -125,34 +79,42 @@ const makeLine = (markers, line, children, options, classNames = []) => {
 };
 
 const wrapLines = function wrapLines(ast, markers, lineCount, options) {
-  function createTree(ast, markers, lineCount, options) {
-    let tree = {};
-
-    function addLineNumbers(ast, a, b) {
-      ast.forEach((astItem, index) => {
-        if (!astItem.used) {
-          for (let i = a; i <= b; i++) {
-            if (
-              astItem.lineNumber === i &&
-              ((astItem.position &&
-                astItem.position.start.line === astItem.position.end.line) ||
-                (!astItem.position && astItem.type === 'text'))
-            ) {
-              if (astItem.type === 'text' && astItem.value === '') {
-              } else {
-                tree[`line-${astItem.lineNumber}`] = tree[
-                  `line-${astItem.lineNumber}`
-                ]
-                  ? [...tree[`line-${astItem.lineNumber}`], astItem]
-                  : [astItem];
-              }
-              ast[index]['used'] = true;
+  let tree = {};
+  function addLineNumbers(ast, a, b) {
+    ast.forEach((astItem, index) => {
+      if (!astItem.used) {
+        for (let i = a; i <= b; i++) {
+          if (
+            (astItem.position &&
+              astItem.position.start.line === astItem.position.end.line &&
+              astItem.position.start.line === i) ||
+            (!astItem.position && astItem.type === 'text')
+          ) {
+            if (astItem.type === 'text' && astItem.value === '') {
+            } else {
+              tree[`line-${i}`] = tree[`line-${i}`]
+                ? [...tree[`line-${i}`], astItem]
+                : [astItem];
             }
-            if (
-              astItem.lineNumber === i &&
-              astItem.position &&
-              astItem.position.start.line !== astItem.position.end.line
-            ) {
+            ast[index]['used'] = true;
+          }
+
+          if (
+            astItem.position &&
+            astItem.position.start.line !== astItem.position.end.line &&
+            astItem.position.start.line === i
+          ) {
+            if (astItem.type === 'text' && astItem.value.indexOf('\n') !== -1) {
+              const textArr = astItem.value.split('\n');
+              textArr.forEach((value, vi) => {
+                if (value !== '') {
+                  tree[`line-${i + vi}`] = tree[`line-${i + vi}`]
+                    ? [...tree[`line-${i + vi}`], { type: 'text', value }]
+                    : [{ type: 'text', value }];
+                }
+              });
+              ast[index]['used'] = true;
+            } else {
               for (
                 let j = astItem.position.start.line;
                 j <= astItem.position.end.line;
@@ -176,88 +138,42 @@ const wrapLines = function wrapLines(ast, markers, lineCount, options) {
             }
           }
         }
-      });
-    }
-
-    addLineNumbers(ast, 1, lineCount);
-
-    return Object.entries(tree)
-      .reduce((acc, [key, values]) => {
-        if (key.includes('class')) {
-          if (acc[+key.replace('class-', '') - 1]) {
-            acc[+key.replace('class-', '') - 1].properties.className
-              ? typeof acc[+key.replace('class-', '') - 1].properties
-                  .className === 'string'
-                ? (acc[+key.replace('class-', '') - 1].properties.className = [
-                    acc[+key.replace('class-', '') - 1].properties.className,
-                    ...values
-                  ])
-                : (acc[+key.replace('class-', '') - 1].properties.className = [
-                    ...acc[+key.replace('class-', '') - 1].properties.className,
-                    ...values
-                  ])
-              : (acc[
-                  +key.replace('class-', '') - 1
-                ].properties.className = values);
-          } else {
-            acc[+key.replace('class-', '') - 1] = values;
-          }
-          return acc;
-        } else {
-          if (
-            acc[+key.replace('line-', '') - 1] &&
-            Array.isArray(acc[+key.replace('line-', '') - 1])
-          ) {
-            acc[+key.replace('line-', '') - 1] = makeLine(
-              markers,
-              +key.replace('line-', '') - 1,
-              values,
-              options,
-              acc[+key.replace('line-', '') - 1]
-            );
-          } else {
-            acc[+key.replace('line-', '') - 1] = makeLine(
-              markers,
-              +key.replace('line-', '') - 1,
-              values,
-              options
-            );
-          }
-          return acc;
-        }
-      }, [])
-      .sort((a, b) => a.lineNumber - b.lineNumber);
+      }
+    });
   }
-  const test = createTree(ast, markers, lineCount, options);
-  return test;
+  addLineNumbers(ast, 1, lineCount);
+
+  let finalTree = [];
+  for (let i = 0; i < lineCount; i++) {
+    finalTree.push(
+      makeLine(
+        markers,
+        i,
+        tree[`line-${i}`] ? tree[`line-${i}`] : [{ type: 'text', value: '' }],
+        options,
+        tree[`class-${i}`]
+      )
+    );
+  }
+
+  return finalTree;
 };
 
 module.exports = function(ast, options) {
-  let { nodes, lineNumber } = lineNumberify(ast);
-
-  if (nodes[nodes.length - 1]) {
-    if (
-      nodes[nodes.length - 1].value === '' &&
-      nodes[nodes.length - 1].type === 'text'
-    ) {
-      nodes.length = nodes.length - 1;
-    }
-  }
-  lineNumber = nodes[nodes.length - 1].lineNumber;
-
-  let lineNumbers = [];
-  for (let i = 1; i <= lineNumber; i++) {
-    lineNumbers.push({ line: i });
+  let lineLength = ast[ast.length - 1]
+    ? ast[ast.length - 1].position ? ast[ast.length - 1].position.end.line : 1
+    : 1;
+  let markers = [];
+  for (let i = 1; i <= lineLength; i++) {
+    markers.push({ line: i });
   }
 
   options.markers &&
     options.markers.forEach(marker => {
-      if (lineNumbers[(marker.line ? marker.line : marker) - 1]) {
-        lineNumbers[(marker.line ? marker.line : marker) - 1][
-          'highlight'
-        ] = true;
+      if (markers[(marker.line ? marker.line : marker) - 1]) {
+        markers[(marker.line ? marker.line : marker) - 1]['highlight'] = true;
       }
     });
 
-  return wrapLines(nodes, lineNumbers, lineNumber, options);
+  return wrapLines(ast, markers, lineLength, options);
 };
